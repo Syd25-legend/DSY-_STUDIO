8531b7e8e44fbf4a9916dc5851e36c8acf38e8b5
Syd25-legend <soudey2508@gmail.com>
2025-08-23 17:02:31 +0530

    sitemap addition

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import sitemap from 'vite-plugin-sitemap' 
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -11,6 +12,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
+    sitemap({ hostname: 'https://studiodsy.shop/' }),
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
