05deb773411eb4c39fb6bb7a548fceb36ac5106b
Syd25-legend <soudey2508@gmail.com>
2025-09-13 21:03:28 +0530

    logo change

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
-import sitemap from 'vite-plugin-sitemap' 
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    sitemap({ hostname: 'https://studiodsy.shop/' }),
+    
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
